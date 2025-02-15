74ac34aff3c7ceeb83da2b94c1678f1291aa42cc
Akshat035 <akshat.kansara@avestatechnologies.com>
2025-02-15 19:43:19 +0530

    docs: Add TypeDoc documentation for components

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Main export file
+export { default as UserManagement } from "./features/user/User";
+export { default as UserTable } from "./features/user/UserTable";
+export { default as UserDelete } from "./features/user/UserDelete";
+export { default as Button } from "./shared/UI/Button";
+export type { IUser } from "./types/user";
